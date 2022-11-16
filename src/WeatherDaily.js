45ace645dba6056eb390cd335616a9ad0d5cf575
Tsepi <100115214+Tsepi@users.noreply.github.com>
2022-11-16 14:24:07 +0200

    Added daily forecast component

--- src/WeatherDaily.js
+++ src/WeatherDaily.js
@@ -1,38 +1,42 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 
+import WeatherDailyForecast from "./WeatherDailyForecast";
 import "./WeatherDaily.css";
 
 export default function WeatherDaily(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setLoaded(true);
+    setForecast(response.data.daily);
   }
-  let latitude = props.coordinates.lat;
-  let longtitude = props.coordinates.lon;
-  let apiKey = `7d88e39fad8e3a2f1b2d1076c46f769c`;
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
+  if (loaded) {
+    console.log(forecast);
+    //let iconURL = `http://openweathermap.org/img/wn/${props.forecast[0].weather[0].icon}@2x.png`;
 
-  return (
-    <div className="WeatherDaily">
-      <div className="container daily">
-        <div className="row ">
-          <hr />
-          <div className="col-2">
-            <div className="daily-forecast">Wed</div>
-            <img
-              src="http://openweathermap.org/img/wn/01d@2x.png"
-              alt=" sunny "
-              width="62px"
-            />
-            <div className="temperatures">
-              <span className="temperature-max"> 23° </span>
-              <span className="temperature-min"> 15° </span>
+    return (
+      <div className="WeatherDaily">
+        <div className="container daily">
+          <div className="row ">
+            <hr />
+            <div className="col">
+              <WeatherDailyForecast data={forecast[0]} />
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let latitude = props.coordinates.lat;
+    let longtitude = props.coordinates.lon;
+    let apiKey = `7d88e39fad8e3a2f1b2d1076c46f769c`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return <p>Searching...</p>;
+  }
 }
---
+++ src/WeatherDailyForecast.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function WeatherDailyForecast(props) {
+  let iconURL = `http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`;
+  console.log(props.data);
+  function maxTemp() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemp() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="daily-forecast">{day()}</div>
+      <img src={iconURL} alt=" sunny " width="62px" />
+      <div className="temperatures">
+        <span className="temperature-max">{maxTemp()}</span>
+        <span className="temperature-min">{minTemp()} </span>
+      </div>
+    </div>
+  );
+}
